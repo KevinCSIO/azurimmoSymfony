78bf5842d014255e75b54696edba6b1e848732a5
KevinCSIO <kevin.camus@sts-sio-caen.info>
2025-03-25 11:16:29 +0100

    ajout list_batiment

---
+++ src/Controller/BatimentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BatimentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Batiment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BatimentController extends AbstractController
+{
+    #[Route('/batiment', name: 'app_batiment')]
+    public function index(): Response
+    {
+        return $this->render('batiment/index.html.twig', [
+            'controller_name' => 'BatimentController',
+        ]);
+    }
+
+    #[Route('/batiment/list', name: 'app_batiment_list')]
+    public function getBatiments(EntityManagerInterface $entityManager): Response
+    {
+        $repository = $entityManager->getRepository(Batiment::class);
+
+        $batiments = $repository->findAll();
+        return $this->render('batiment/list.html.twig', [
+            'batiments' => $batiments,
+        ]);
+    }
+}
---
+++ src/Entity/Batiment.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BatimentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BatimentRepository::class)]
+class Batiment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+}
