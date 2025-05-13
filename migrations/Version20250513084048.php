2def34ea5985101719dd4cf585878f13f35fb6b1
KevinCSIO <kevin.camus@sts-sio-caen.info>
2025-05-13 10:48:41 +0200

    ajout design sur crud_batiment

---
+++ migrations/Version20250513084048.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250513084048 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE contrat (id INT AUTO_INCREMENT NOT NULL, appartement_id INT NOT NULL, date_entree DATE NOT NULL, date_sortie DATE NOT NULL, montant_loyer DOUBLE PRECISION NOT NULL, montant_charges DOUBLE PRECISION NOT NULL, statut VARCHAR(255) NOT NULL, INDEX IDX_60349993E1729BBA (appartement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE contrat ADD CONSTRAINT FK_60349993E1729BBA FOREIGN KEY (appartement_id) REFERENCES appartement (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contrat DROP FOREIGN KEY FK_60349993E1729BBA');
+        $this->addSql('DROP TABLE contrat');
+    }
+}
--- src/Controller/BatimentController.php
+++ src/Controller/BatimentController.php
@@ -2,32 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Batiment;
+use App\Form\BatimentType;
 use App\Repository\BatimentRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Batiment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class
-BatimentController extends AbstractController
+#[Route('/batiment')]
+final class BatimentController extends AbstractController
 {
-    #[Route('/batiment', name: 'app_batiment')]
-    public function index(): Response
+    #[Route(name: 'app_batiment_index', methods: ['GET'])]
+    public function index(BatimentRepository $batimentRepository): Response
     {
         return $this->render('batiment/index.html.twig', [
-            'controller_name' => 'BatimentController',
+            'batiments' => $batimentRepository->findAll(),
         ]);
     }
 
-    #[Route('/batiment/list', name: 'app_batiment_list')]
-    public function getBatiments(EntityManagerInterface $entityManager): Response
+    #[Route('/new', name: 'app_batiment_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $repository = $entityManager->getRepository(Batiment::class);
+        $batiment = new Batiment();
+        $form = $this->createForm(BatimentType::class, $batiment);
+        $form->handleRequest($request);
 
-        $batiments = $repository->findAll();
-        return $this->render('batiment/list.html.twig', [
-            'batiments' => $batiments,
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($batiment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_batiment_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('batiment/new.html.twig', [
+            'batiment' => $batiment,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_batiment_show', methods: ['GET'])]
+    public function show(Batiment $batiment): Response
+    {
+        return $this->render('batiment/show.html.twig', [
+            'batiment' => $batiment,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_batiment_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Batiment $batiment, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BatimentType::class, $batiment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_batiment_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('batiment/edit.html.twig', [
+            'batiment' => $batiment,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_batiment_delete', methods: ['POST'])]
+    public function delete(Request $request, Batiment $batiment, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$batiment->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($batiment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_batiment_index', [], Response::HTTP_SEE_OTHER);
     }
 }
